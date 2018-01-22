22492d5a4a9f9c008d0f7c16a0492c6ee0a86f27
Bartô Alheiros <alheirosb@gmail.com>
2018-01-22 02:03:07 -0300

    Atualização nas pastas: criada pasta resource contendo os arquivos para apresentar no projeto. - Script_Role_Grants e o dum foi movido para resources>dump

---
+++ resource/role_grants/Script_GRANTS.sql
@@ -0,0 +1,25 @@
+use assistech;
+
+CREATE ROLE 
+'administrador';
+
+GRANT ALL ON
+assistech.* TO 'administrador';
+
+CREATE ROLE 
+'funcionario';
+
+CREATE ROLE
+'supervisor';
+
+CREATE ROLE
+'tecnico';
+
+CREATE ROLE
+'atendente';
+
+
+
+select * from mysql.user;
+
+#drop role administrador;
