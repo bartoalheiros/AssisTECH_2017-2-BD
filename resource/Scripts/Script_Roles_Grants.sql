5b98f155f91396d8a7abe4f6546731dc6c646920
B.Alheiros <alheirosb@gmail.com>
2018-02-02 15:18:24 -0300

    Script Roles att.

--- resource/Scripts/Script_Roles_Grants.sql
+++ resource/Scripts/Script_Roles_Grants.sql
@@ -1,10 +1,14 @@
 use assistech;
 
-CREATE ROLE 
-'administrador';
+CREATE ROLE administrador;
 
-GRANT ALL ON
+GRANT INSERT, SELECT, UPDATE, DELETE ON
 assistech.* TO 'administrador';
+
+CREATE USER 'administrador1'@'localhost' IDENTIFIED BY 'admin1';
+#CREATE USER 'read_user1'@'localhost' IDENTIFIED BY 'read_user1pass';
+#CREATE USER 'read_user2'@'localhost' IDENTIFIED BY 'read_user2pass';
+#CREATE USER 'rw_user1'@'localhost' IDENTIFIED BY 'rw_user1pass';
 
 CREATE ROLE 
 'funcionario';
@@ -22,6 +26,8 @@ CREATE ROLE
 'atendente';
 
 
-#select * from mysql.user;
+select * from mysql.user;
+
+select current_role();
 
 #drop role adm_financ;
