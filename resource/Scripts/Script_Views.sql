52448c55065d615861c8a856141e624ef6b16e39
Lucas Wagner <jeronimocastromelo@gmail.com>
2018-02-08 22:45:57 -0200

    views atualizada

--- resource/Scripts/Script_Views.sql
+++ resource/Scripts/Script_Views.sql
@@ -13,4 +13,12 @@ CREATE VIEW os as SELECT *FROM ordem_servico;
 
 CREATE VIEW equi_datas as SELECT *FROM equipamento e WHERE e.data_entrada between ('2012-11-10') and ('2012-11-18');
 
+CREATE VIEW OS_Fatura_andamento as SELECT os.Num_Ordem_Servico, os.Dt_devida, os.Prazo_em_dias, os.Cod_orcamento, os.Cod_fatura, f.Valor_total, f.Cod_cliente FROM ordem_servico as os INNER JOIN fatura as f on os.Cod_fatura = f.Cod WHERE f.Status_ = 'Em andamento';
+
+SELECT *FROM funcionario as f;
+
+CREATE VIEW jornada_fun as SELECT *FROM jornada_de_trabalho as jt INNER JOIN funcionario as f on jt.id = f.idJornada WHERE jt.Trabalha_sabado = 'Sim' and f.CargaHoraria = 8;
+
+SELECT *FROM kpi;
+
 
